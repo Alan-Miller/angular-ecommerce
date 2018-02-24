b651eb5fd5001bc814b085a4498c0b4011aaf509
Alan-Miller <alan.ashman@gmail.com>
2018-02-24 15:54:18 -0700

    edits

--- app/app.js
+++ app/app.js
@@ -1,15 +1,19 @@
 angular.module('ecommerce', ['ui.router'])
-  .config(function($stateProvider, $urlRouterProvider){
+  .config(function ($stateProvider, $urlRouterProvider) {
     $stateProvider.state('home', {
-      templateUrl:'app/views/home/home.html',
+      templateUrl: 'app/views/home/home.html',
 
     }).state('components', {
-      url:'/components',
-      templateUrl:'app/views/components/components.html',
-      controller:'componentsCtrl'
+      url: '/components',
+      templateUrl: 'app/views/components/components.html',
+      controller: 'componentsCtrl'
+    // }).state('home', {
+      // url: '/home',
+      // templateUrl: 'app/views/components/components.html',
+      // controller: 'componentsCtrl'
     }).state('products', {
-      url:'/products',
-      templateUrl:'app/views/components/components.html',
+      url: '/products',
+      templateUrl: 'app/views/components/components.html',
       // controller:'productsCtrl'
     })
 
